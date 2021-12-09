d9fb192390665e34045700a0a5ef4fd5a3e133ea
Nelson Rueda <ruedachaconnelson@gmail.com>
2021-12-09 10:05:09 -0500

    Initial commit

---
+++ app/src/main/java/com/wposs/retrofitexercise/retrofit/MainActivity.java
@@ -0,0 +1,16 @@
+package com.wposs.retrofitexercise.retrofit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.wposs.retrofitexercise.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
